a76f3f45d290fca314f20854111271a9edda01c8
Asianzu Henry <asianzuhenry@gmail.com>
2024-08-23 19:43:25 +0300

    new branch layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
 console.log('Server is running...');
 
 let yr = new Date().getFullYear()
-
-
 const Year = document.querySelector(".year").innerText = yr
+let isVisible = false
 
+const ToggleNavigation = () => {
+    const Menu = document.querySelector("#nav")
 
-
+    isVisible ? isVisible = false : isVisible = true
+    isVisible ? Menu.style.display = "block" : Menu.style.display = ''
+}
