db005c6a7e437f12a44fbcfe8003d01997b074d0
Asianzu Henry <asianzuhenry@gmail.com>
2024-08-24 09:23:53 +0300

    header navigation toggle button

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,14 @@
 console.log('Server is running...');
 
 let yr = new Date().getFullYear()
-const Year = document.querySelector(".year").innerText = yr
+// const Year = document.querySelector(".year").innerText = yr
 let isVisible = false
 
 const ToggleNavigation = () => {
-    const Menu = document.querySelector("#nav")
+    console.log("clcked");
+    
+    // const Menu = document.querySelector("#nav")
 
-    isVisible ? isVisible = false : isVisible = true
-    isVisible ? Menu.style.display = "block" : Menu.style.display = ''
+    // isVisible ? isVisible = false : isVisible = true
+    // isVisible ? Menu.style.display = "block" : Menu.style.display = ''
 }
